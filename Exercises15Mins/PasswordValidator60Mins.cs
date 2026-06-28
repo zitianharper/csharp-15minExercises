0e2ab6031284de8cdaad473da3078ae9690b3ed5
Zitian Harper <Zitian.harper@icloud.com>
2026-06-28 22:58:55 +0100

    completed passwordValidator

---
+++ Exercises15Mins/PasswordValidator60Mins.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises15Mins
+{
+    public class PasswordValidator60Mins
+    {
+        public bool PassWordValidatorMethod(string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            if (input == "")
+                return false;
+
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            if (input.Length < 8)
+                return false;
+
+            if (!input.Any(c => char.IsUpper(c)))
+                return false;
+
+            if (!input.Any(c => char.IsLower(c)))
+                return false;
+
+            if (!input.Any(c => char.IsDigit(c)))
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Tests/PasswordValidator60MinsTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercises15Mins;
+using Shouldly;
+
+namespace Tests
+{
+    internal class PasswordValidator60MinsTest
+    {
+        [Test]
+        public void ThrowWhenNull()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string? input = null;
+
+            //act + assert
+            Should.Throw<ArgumentException>(() => bl.PassWordValidatorMethod(input));
+        }
+
+        [Test]
+        public void ReturnFalseWhenEmpty()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string input = "";
+
+            //act
+            var result = bl.PassWordValidatorMethod(input);
+            //assert
+            result.ShouldBeFalse();
+
+        }
+
+        [Test]
+        public void ReturnFalseWhen7Chars()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string input = "12345Up";
+
+            //act
+            var result = bl.PassWordValidatorMethod(input);
+            //assert
+            result.ShouldBeFalse();
+
+        }
+
+        [Test]
+        public void ReturnTrueWhen8Chars()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string input = "12345Up8";
+
+            //act
+            var result = bl.PassWordValidatorMethod(input);
+            //assert
+            result.ShouldBeTrue();
+
+        }
+        [Test]
+        public void ReturnFalseWhenNoUpper()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string input = "123456up";
+
+            //act
+            var result = bl.PassWordValidatorMethod(input);
+            //assert
+            result.ShouldBeFalse();
+
+        }
+
+        [Test]
+        public void ReturnTrueWhenOneUpper()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string input = "123456Up";
+
+            //act
+            var result = bl.PassWordValidatorMethod(input);
+            //assert
+            result.ShouldBeTrue();
+
+        }
+
+        [Test]
+        public void ReturnFalseWhenNoDigits()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string input = "qwertyUi";
+
+            //act
+            var result = bl.PassWordValidatorMethod(input);
+            //assert
+            result.ShouldBeFalse();
+
+        }
+
+        [Test]
+        public void ReturnTrueWhenOneDigit()
+        {
+            //assign
+            var bl = new PasswordValidator60Mins();
+            string input = "1pppppUp";
+
+            //act
+            var result = bl.PassWordValidatorMethod(input);
+            //assert
+            result.ShouldBeTrue();
+
+        }
+    }
+}
